86bd7bb7ff626aa6865230e356f29d7ba1c87b86
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-03-12 14:50:46 -0500

    Set XDG environment

---
+++ roles/_pre-create_environment/files/compare.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+systemd_env="$(systemctl --user show-environment)"
+readonly systemd_env
+
+while read -r var; do
+    if [ ! "$(printf '%s' "${var}" | cut -c 1)" = '#' ]; then
+        grep -x "$(eval "printf '%s' \"${var}\"")" > /dev/null << EOF || exit 1
+${systemd_env}
+EOF
+    fi
+done < "${1}"
