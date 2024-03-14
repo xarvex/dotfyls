a8be6b1e5dc3a459eb863009bd52796ee6c7dd22
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-03-13 22:08:52 -0500

    Handle potential overriding and appending/prepending of environment variables

--- roles/_pre-create_environment/files/compare.sh
+++ roles/_pre-create_environment/files/compare.sh
@@ -1,14 +1,34 @@
 #!/bin/sh
 
-systemd_env="$(systemctl --user show-environment)"
-readonly systemd_env
+output=.applied
+while getopts "o:" opt; do
+ case ${opt} in
+    o)
+        output="${OPTARG}"
+        ;;
+    *)
+        ;;
+ esac
+done
+shift $((OPTIND - 1))
+: > "${output}".tmp
 
+latest=0
 for arg in "${@}"; do
     while read -r var; do
-        if [ ! "$(printf '%s' "${var}" | cut -c 1)" = '#' ]; then
-            grep -x "$(eval "printf '%s' \"${var}\"")" > /dev/null << EOF || exit 1
-${systemd_env}
-EOF
+        if [ "$(printf '%s' "${var}" | cut -c 1)" != '#' ] && [ "${var#*=}" != "${var}" ]; then
+            printf '%s\n' "${var}" >> "${output}".tmp
         fi
     done < "${arg}"
+
+    timestamp="$(stat -c %Z "${arg}")"
+    [ "${timestamp}" -gt "${latest}" ] && latest="${timestamp}"
 done
+
+if [ "${latest}" -lt "$(stat -c %Z /proc)" ]; then
+    mv "${output}".tmp "${output}"
+elif [ -f "${output}" ] && diff --brief "${output}".tmp "${output}" > /dev/null; then
+    rm "${output}".tmp
+else
+    exit 1
+fi
