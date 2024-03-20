d650a2bbe25240e99a3c085984084086584c50dd
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-03-19 20:03:10 -0500

    Add Git aliases

--- alias.zsh
+++ alias.zsh
@@ -5,6 +5,15 @@ fi
 if (( ${+commands[fastfetch]} )); then
     alias neofetch=fastfetch
     alias fetch=fastfetch
+
+if (( ${+commands[git]} )); then
+    alias ga='git add'
+    alias gc='git commit'
+    alias gd='git diff'
+    alias gds='git diff --staged'
+    alias gp='git push'
+    alias gs='git status'
+    alias gup='git log -p @{push}.. $@'
 fi
 
 if (( ${+commands[ls]} )); then
