7a22d9a8372f8d8fa75db721ad989b192f5364df
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-21 01:36:56 -0500

    fix(vm): spice-agent service condition

--- host/botworks-virtualized/default.nix
+++ host/botworks-virtualized/default.nix
@@ -10,9 +10,9 @@
   systemd.user.services.spice-agent = {
     enable = true;
     wantedBy = [ "graphical-session.target" ];
-    serviceConfig.ExecStart = "${lib.getExe' pkgs.spice-vdagent "spice-vdagent"} -x ";
+    serviceConfig.ExecStart = "${lib.getExe' pkgs.spice-vdagent "spice-vdagent"} -x";
     unitConfig = {
-      ConditionVirtualization = "botworks-virtualized";
+      ConditionVirtualization = "vm";
       Description = [ "Spice guest session agent" ];
       After = [ "graphical-session-pre.target" ];
       PartOf = [ "graphical-session.target" ];
