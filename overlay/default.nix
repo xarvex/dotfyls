c045d8f95a9c59d908236d591569f6cfc319d7e0
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-21 03:33:47 -0500

    fix(WezTerm): get working on Hyprland
    See [#5264](https://github.com/wez/wezterm/pull/5264)

--- overlay/default.nix
+++ overlay/default.nix
@@ -9,6 +9,12 @@
           ./fastfetch-nixos-small.patch
         ];
       });
+
+      # until release made and available
+      wezterm = prev.wezterm.overrideAttrs (o: {
+        # https://github.com/wez/wezterm/pull/5264
+        patches = (o.patches or [ ]) ++ [ ./wezterm-5264.patch ];
+      });
     })
   ];
 }
