181ef31e21b1d53941e43eca3816eba1da1a7c6e
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-23 02:59:36 -0500

    feat(desktop)!: use SDDM

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,12 +1,28 @@
 { config, lib, pkgs, ... }:
 
-lib.mkMerge [
-  (lib.mkIf config.hm.custom.desktop.hyprland.enable {
-    programs.hyprland.enable = true;
+{
+  options.custom.desktop.sddm.enable = lib.mkEnableOption "Enable SDDM" // { default = true; };
 
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-    };
-  })
-]
+  config = lib.mkMerge [
+    (lib.mkIf config.custom.desktop.sddm.enable {
+      environment.systemPackages = with pkgs; [
+        (catppuccin-sddm.override { flavor = "mocha"; })
+      ];
+
+      services.displayManager.sddm = {
+        enable = true;
+        wayland.enable = true;
+        package = pkgs.kdePackages.sddm;
+        theme = "catppuccin-mocha";
+      };
+    })
+    (lib.mkIf config.hm.custom.desktop.hyprland.enable {
+      programs.hyprland.enable = true;
+
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+      };
+    })
+  ];
+}
