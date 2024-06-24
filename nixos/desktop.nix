d0c699a46e4f56b31b1aa88d10bd5a338435b63f
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-24 00:47:36 -0500

    fix(desktop): generate default state for SDDM

--- nixos/desktop.nix
+++ nixos/desktop.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, user, ... }:
 
 {
   options.custom.desktop.sddm.enable = lib.mkEnableOption "Enable SDDM" // { default = true; };
@@ -15,6 +15,13 @@
         package = pkgs.kdePackages.sddm;
         theme = "catppuccin-mocha";
       };
+
+      systemd.tmpfiles.rules = [
+        "d /var/lib/sddm 0755 root root - -"
+
+        # Not really last, but allows specifying as default user on each boot
+        "f+ /var/lib/sddm/state.conf - - - - ${builtins.replaceStrings ["\n"] ["\\n"] (lib.generators.toINI { } { Last.User = user; })}"
+      ];
     })
     (lib.mkIf config.hm.custom.desktop.hyprland.enable {
       programs.hyprland.enable = true;
