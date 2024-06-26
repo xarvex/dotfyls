fc9e6ffb2c13dbfe7bf2f6a5ed15956dc8c2d1dd
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-25 21:56:57 -0500

    fix(users): allow persistent password

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,18 +1,27 @@
-{ user, ... }:
+{ lib, user, ... }:
 
 {
-  users = {
-    mutableUsers = false;
+  options.warnings = lib.mkOption {
+    apply = lib.filter (
+      w: !(lib.strings.hasInfix "The options silently discard others by the order of precedence" w)
+    );
+  };
 
+  config = {
     users = {
-      root = {
-        initialPassword = "password";
-      };
+      mutableUsers = false;
+      users = {
+        root = {
+          initialPassword = "password";
+          hashedPasswordFile = "/persist/etc/shadow/root";
+        };
 
-      ${user} = {
-        isNormalUser = true;
-        initialPassword = "password";
-        extraGroups = [ "networkmanager" "wheel" ];
+        ${user} = {
+          isNormalUser = true;
+          initialPassword = "password";
+          hashedPasswordFile = "/persist/etc/shadow/${user}";
+          extraGroups = [ "networkmanager" "wheel" ];
+        };
       };
     };
   };
