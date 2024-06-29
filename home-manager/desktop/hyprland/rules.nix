b1b891afd7a26fcac12b59d34fb4cbe923196815
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-29 17:19:59 -0500

    fix(desktop): use mkIf for configs

--- home-manager/desktop/hyprland/rules.nix
+++ home-manager/desktop/hyprland/rules.nix
@@ -1,6 +1,6 @@
 { config, lib, ... }:
 
-{
+lib.mkIf config.custom.desktop.hyprland.enable {
   wayland.windowManager.hyprland.settings.windowrulev2 = [ ]
     ++ lib.optionals config.custom.programs.xwaylandvideobridge.enable [
     "opacity 0.0 override,class:^(xwaylandvideobridge)$"
