6db5b4907e20d1eb21273c9e4a91ae2313b584e5
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-06-30 21:38:09 -0500

    fix(nixos): ensure same values

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,32 +1,28 @@
 { pkgs, ... }:
 
 {
-  nix =
-    let
-      nixPath = [
-        "nixpkgs=flake:nixpkgs"
-        "/nix/var/nix/profiles/per-user/root/channels"
-      ];
-    in
-    {
-      inherit nixPath;
+  nix = rec {
+    nixPath = [
+      "nixpkgs=flake:nixpkgs"
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
 
-      channel.enable = false;
+    channel.enable = false;
 
-      package = pkgs.nixVersions.latest;
+    package = pkgs.nixVersions.latest;
 
-      gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 14d";
-      };
-
-      settings = {
-        nix-path = nixPath;
-
-        auto-optimise-store = true;
-        use-xdg-base-directories = true;
-        experimental-features = [ "nix-command" "flakes" ];
-      };
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 14d";
     };
+
+    settings = {
+      nix-path = nixPath;
+
+      auto-optimise-store = true;
+      use-xdg-base-directories = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+  };
 }
