b6c12d26a85a0973a61efe551bfcdfbe376f17b5
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-01 21:34:21 -0500

    feat(installer): provide install command

--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -1,7 +1,14 @@
-{ modulesPath, ... }:
+{ modulesPath, pkgs, ... }:
 
 {
   imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ];
+
+  environment.systemPackages = [
+    (pkgs.writeShellApplication {
+      name = "dotfyls-install";
+      text = builtins.readFile ../../install.sh;
+    })
+  ];
 
   nixpkgs.hostPlatform = "x86_64-linux";
 }
