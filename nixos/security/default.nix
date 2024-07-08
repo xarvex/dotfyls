f37528e600978eeb310bb8fb485c414f7341ef00
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-07 22:10:04 -0500

    docs(security): add additional resources

--- nixos/security/default.nix
+++ nixos/security/default.nix
@@ -6,7 +6,10 @@
 # Resouces used:
 #   https://nixos.wiki/wiki/Security
 #   https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/profiles/hardened.nix
+#   https://madaidans-insecurities.github.io/guides/linux-hardening.html
+#   https://github.com/a13xp0p0v/kernel-hardening-checker
 #   https://dataswamp.org/~solene/2022-01-13-nixos-hardened.html
+#   https://medium.com/@ganga.jaiswal/88bb7d77ba22
 { ... }:
 
 {
