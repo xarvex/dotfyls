659234a9c9ca732944d82e084c1d8468d76d5d03
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-11 01:30:23 -0500

    fix!: use overrides for system configurations

--- .zshenv
+++ .zshenv
@@ -1,5 +1,5 @@
-# shellcheck disable=SC2086,SC2206
+# shellcheck disable=SC2034,SC2206
 
-source ${ZDOTDIR:-${HOME}}/common/env-extra.zsh
+skip_global_compinit=1
 
 fpath=( ${ZDOTDIR:-${HOME}}/functions ${fpath[@]} )
--- flake.nix
+++ flake.nix
@@ -25,82 +25,97 @@
         };
       };
 
-    flake.homeManagerModules = rec {
-      default = zsh;
+    flake = {
+      nixosModules = rec {
+        default = zsh;
 
-      zsh = ({ config, lib, pkgs, ... }: lib.mkIf config.programs.zsh.enable (
-        let
-          common = path: builtins.readFile ./common/${path};
-        in
-        {
+        zsh = { config, lib, pkgs, ... }: lib.mkIf config.programs.zsh.enable {
           programs.zsh = {
-            dotDir = ".config/zsh";
-
-            envExtra = common "env-extra.zsh";
-
-            # Everything below becomes generated .zshrc:
-
-            initExtraFirst = common "init-extra-first.zsh";
-
-            defaultKeymap = "viins";
-            localVariables = {
-              HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = 1;
-            };
-
-            initExtraBeforeCompInit = common "init-extra-before-comp-init.zsh";
-
-            enableCompletion = true;
-            completionInit = common "completion-init.zsh";
-
-            autosuggestion.enable = true;
-
-            # Plugin path and fpath is added earlier, but here is where sourced:
-            plugins = [
-              {
-                name = "powerlevel10k";
-                src = pkgs.zsh-powerlevel10k;
-                file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-              }
-              {
-                name = "vi-mode";
-                file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
-                src = pkgs.zsh-vi-mode;
-              }
-              {
-                name = "you-should-use";
-                file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
-                src = pkgs.zsh-you-should-use;
-              }
-              {
-                name = "fast-syntax-highlighting";
-                file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-                src = pkgs.zsh-fast-syntax-highlighting;
-              }
-            ];
-
-            history = rec {
-              size = 10000;
-              save = size;
-              path = "${config.xdg.stateHome}/zsh/history";
-
-              ignoreDups = true;
-              ignoreAllDups = true;
-              ignoreSpace = true;
-              share = true;
-            };
-            autocd = true;
-
-            initExtra = lib.concatStringsSep "\n" [
-              (common "init-extra.zsh")
-              (common "p10k.zsh")
-            ];
-
-            syntaxHighlighting.enable = false; # Use zsh-fast-syntax-highlighting.
-            historySubstringSearch.enable = true;
+            enableCompletion = false;
+            enableGlobalCompInit = false;
           };
-        }
-      ));
+
+          environment.pathsToLink = [ "/share/zsh" ];
+        };
+      };
+      homeManagerModules = rec {
+        default = zsh;
+
+        zsh = ({ config, lib, pkgs, ... }: lib.mkIf config.programs.zsh.enable (
+          let
+            common = path: builtins.readFile ./common/${path};
+          in
+          {
+            programs.zsh = {
+              dotDir = ".config/zsh";
+              # Everything below becomes generated .zshrc:
+
+              initExtraFirst = common "init-extra-first.zsh";
+
+              defaultKeymap = "viins";
+              localVariables = {
+                HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = 1;
+              };
+
+              initExtraBeforeCompInit = common "init-extra-before-comp-init.zsh";
+
+              enableCompletion = true;
+              completionInit = common "completion-init.zsh";
+
+              autosuggestion.enable = true;
+
+              # Plugin path and fpath is added earlier, but here is where sourced:
+              plugins = [
+                {
+                  name = "powerlevel10k";
+                  src = pkgs.zsh-powerlevel10k;
+                  file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+                }
+                {
+                  name = "vi-mode";
+                  file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+                  src = pkgs.zsh-vi-mode;
+                }
+                {
+                  name = "you-should-use";
+                  file = "share/zsh/plugins/you-should-use/you-should-use.plugin.zsh";
+                  src = pkgs.zsh-you-should-use;
+                }
+                {
+                  name = "completions";
+                  file = "share/zsh-completions/zsh-completions.zsh";
+                  src = pkgs.zsh-completions.src;
+                }
+                {
+                  name = "fast-syntax-highlighting";
+                  file = "share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
+                  src = pkgs.zsh-fast-syntax-highlighting;
+                }
+              ];
+
+              history = rec {
+                size = 10000;
+                save = size;
+                path = "${config.xdg.stateHome}/zsh/history";
+
+                ignoreDups = true;
+                ignoreAllDups = true;
+                ignoreSpace = true;
+                share = true;
+              };
+              autocd = true;
+
+              initExtra = lib.concatStringsSep "\n" [
+                (common "init-extra.zsh")
+                (common "p10k.zsh")
+              ];
+
+              syntaxHighlighting.enable = false; # Use zsh-fast-syntax-highlighting.
+              historySubstringSearch.enable = true;
+            };
+          }
+        ));
+      };
     };
   };
 }
-
