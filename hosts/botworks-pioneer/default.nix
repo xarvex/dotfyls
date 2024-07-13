d7c651a7980f3261af33d6e78e3a02d8dd61fa38
Xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-13 16:09:31 -0500

    fix(pioneer): resolve CPU issue

--- hosts/botworks-pioneer/default.nix
+++ hosts/botworks-pioneer/default.nix
@@ -12,6 +12,10 @@
 
   hardware.enableRedistributableFirmware = true;
 
+  # Intel Bay Trail CPU bug.
+  # See: https://medium.com/@dibyadas/b20281e4b2e2
+  boot.kernelParams = [ "intel_idle.max_cstate=1" ];
+
   networking.hostId = "3540bf30";
   boot.zfs.devNodes = "/dev/disk/by-partuuid";
 }
