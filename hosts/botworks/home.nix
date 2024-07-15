477a478a15cf344cd6d633406189ec353602e60d
xarvex <gitlab-github.8qs1z@slmail.me>
2024-07-15 02:10:18 -0500

    fix(botworks): use 4K and high-refresh

--- hosts/botworks/home.nix
+++ hosts/botworks/home.nix
@@ -1,3 +1,11 @@
 { ... }: {
-  dotfyls.defaultTerminal = "alacritty"; # WezTerm breaks on NVIDIA.
+  dotfyls = {
+    defaultTerminal = "alacritty"; # WezTerm breaks on NVIDIA.
+    desktop.displays = [{
+      name = "DP-2";
+      width = 3840;
+      height = 2160;
+      refresh = 144;
+    }];
+  };
 }
