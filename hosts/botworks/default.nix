46218fdba72aa5166dfb94e38f15b30a2154532a
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-12 13:28:13 -0500

    feat(openrgb): boot with profile

--- hosts/botworks/default.nix
+++ hosts/botworks/default.nix
@@ -5,6 +5,8 @@
     graphics.nvidia.enable = true;
     security.harden.kernel.enable = false;
     filesystems.zfs.unstable = true;
+
+    programs.openrgb.bootProfile = ./openrgb/Overglow.orp;
   };
 
   hardware.enableRedistributableFirmware = true;
--- nixos/programs/openrgb.nix
+++ nixos/programs/openrgb.nix
@@ -11,7 +11,14 @@ in
       [ "services" "hardware" "openrgb" ])
   ];
 
-  options.dotfyls.programs.openrgb.enable = lib.mkEnableOption "OpenRGB" // { default = hmCfg.enable; };
+  options.dotfyls.programs.openrgb = {
+    enable = lib.mkEnableOption "OpenRGB" // { default = hmCfg.enable; };
+    bootProfile = lib.mkOption {
+      type = with lib.types; nullOr path;
+      default = null;
+      description = "OpenRGB profile to start on boot.";
+    };
+  };
 
   config = lib.mkIf cfg.enable {
     dotfyls.persist.directories = [ "/var/lib/OpenRGB" ];
@@ -20,6 +27,12 @@ in
       enable = true;
       package = lib.mkDefault pkgs.openrgb-with-all-plugins;
     };
+
+    systemd.services.openrgb.serviceConfig.ExecStart = lib.mkForce ''
+      ${lib.getExe cfg.package} --server \
+        --server-port ${toString config.services.hardware.openrgb.server.port} \
+        --profile ${cfg.bootProfile}
+    '';
 
     boot.kernelModules = [ "i2c-dev" ];
 
