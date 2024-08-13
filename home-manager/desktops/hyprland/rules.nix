085c76084959fa88f87619793e0fb1ab70a3d8e9
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-12 23:21:30 -0500

    nit(hyprland): use cfg shorthand

--- home-manager/desktops/hyprland/rules.nix
+++ home-manager/desktops/hyprland/rules.nix
@@ -1,6 +1,9 @@
 { config, lib, ... }:
 
-lib.mkIf (config.dotfyls.desktops.enable && config.dotfyls.desktops.desktops.hyprland.enable) {
+let
+  cfg = config.dotfyls.desktops.desktops.hyprland;
+in
+lib.mkIf (config.dotfyls.desktops.enable && cfg.enable) {
   wayland.windowManager.hyprland.settings.windowrulev2 = [ ]
     ++ lib.optionals config.dotfyls.programs.firefox.enable [
     "float, class:^(firefox)$, title:^(Picture-in-Picture)$"
