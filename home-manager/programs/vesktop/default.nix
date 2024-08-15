f5b47ffdbd74bd0565463d67a355e5406a68250f
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-15 02:13:33 -0500

    feat(vesktop): declaratively configure

--- home-manager/programs/vesktop/default.nix
+++ home-manager/programs/vesktop/default.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, self, ... }:
 
 let
   cfg = config.dotfyls.programs.vesktop;
@@ -7,11 +7,33 @@ in
   options.dotfyls.programs.vesktop = {
     enable = lib.mkEnableOption "Vesktop" // { default = config.dotfyls.desktops.enable; };
     package = lib.mkPackageOption pkgs "Vesktop" { default = "vesktop"; };
+    finalPackage = self.lib.mkFinalPackageOption "Vesktop" // {
+      default = cfg.package.overrideAttrs (o:
+        let
+          createState = pkgs.lib.dotfyls.mkCommand {
+            runtimeInputs = with pkgs; [ coreutils ];
+            text = ''
+              cat ${./state.json} > "${config.xdg.configHome}"/vesktop/state.json
+            '';
+          };
+        in
+        {
+          nativeBuildInputs = o.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+          postFixup = (o.postFixup or "") + ''
+            wrapProgram $out/bin/vesktop --run "${lib.getExe createState}"
+          '';
+        });
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with cfg; [ package ];
-
     dotfyls.persist.cacheDirectories = [ ".config/vesktop/sessionData" ];
+
+    home.packages = with cfg; [ finalPackage ];
+
+    xdg.configFile = {
+      "vesktop/settings.json".source = ./settings.json;
+      "vesktop/settings/settings.json".source = ./settings/settings.json;
+    };
   };
 }
