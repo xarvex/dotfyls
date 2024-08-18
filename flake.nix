0c829de994bcb1705819bc5d23f0bbc17ca950fa
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-17 20:41:24 -0500

    feat(flake): initial options

--- flake.nix
+++ flake.nix
@@ -28,83 +28,96 @@
     flake.homeManagerModules = rec {
       default = zsh;
 
-      zsh = ({ config, lib, pkgs, ... }: lib.mkIf config.programs.zsh.enable (
+      zsh = { config, lib, pkgs, ... }:
         let
+          cfg' = config.programs.zsh;
+          cfg = cfg'.dotfyls;
+
           common' = path: ./common/${path};
           common = path: builtins.readFile (common' path);
 
           # From package source let-in:
-          cfg' = config.programs.zsh;
           relToDotDir = file: (lib.optionalString (cfg'.dotDir != null) (cfg'.dotDir + "/")) + file;
           zdotdir = "$HOME/" + lib.escapeShellArg cfg'.dotDir;
         in
         {
-          home.file."${relToDotDir ".p10k.zsh"}".source = common' "p10k.zsh";
-
-          programs.zsh = {
-            dotDir = ".config/zsh";
-
-            envExtra = common "env-extra.zsh";
-
-            # Everything below becomes generated .zshrc:
-
-            initExtraFirst = common "init-extra-first.zsh";
-
-            defaultKeymap = "viins";
-            localVariables = {
-              HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = 1;
+          options.programs.zsh.dotfyls = {
+            enable = lib.mkEnableOption "Zsh dotfyls";
+            historySize = lib.mkOption {
+              type = lib.types.int;
+              default = 10000;
+              example = 100000;
+              description = "Number of history lines.";
             };
-
-            initExtraBeforeCompInit = common "init-extra-before-comp-init.zsh";
-
-            enableCompletion = true;
-            completionInit = common "completion-init.zsh";
-
-            autosuggestion.enable = true;
-
-            # Plugin path and fpath is added earlier, but here is where sourced:
-            plugins = [
-              {
-                name = "powerlevel10k";
-                file = "powerlevel10k.zsh-theme";
-                src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
-              }
-              {
-                name = "zsh-vi-mode";
-                src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
-              }
-              {
-                name = "you-should-use";
-                src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
-              }
-              {
-                name = "fast-syntax-highlighting";
-                src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
-              }
-            ];
-
-            history = rec {
-              size = 10000;
-              save = size;
-              path = "${config.xdg.stateHome}/zsh/history";
-
-              ignoreDups = true;
-              ignoreAllDups = true;
-              ignoreSpace = true;
-              share = true;
-            };
-            autocd = true;
-
-            initExtra = lib.concatStringsSep "\n\n" [
-              (common "init-extra.zsh")
-              "source ${zdotdir}/.p10k.zsh"
-            ];
-
-            syntaxHighlighting.enable = false; # Use zsh-fast-syntax-highlighting.
-            historySubstringSearch.enable = true;
           };
-        }
-      ));
+
+          config = lib.mkIf (cfg'.enable && cfg.enable) {
+            home.file."${relToDotDir ".p10k.zsh"}".source = common' "p10k.zsh";
+
+            programs.zsh = {
+              dotDir = ".config/zsh";
+
+              envExtra = common "env-extra.zsh";
+
+              # Everything below becomes generated .zshrc:
+
+              initExtraFirst = common "init-extra-first.zsh";
+
+              defaultKeymap = "viins";
+              localVariables = {
+                HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE = 1;
+              };
+
+              initExtraBeforeCompInit = common "init-extra-before-comp-init.zsh";
+
+              enableCompletion = true;
+              completionInit = common "completion-init.zsh";
+
+              autosuggestion.enable = true;
+
+              # Plugin path and fpath is added earlier, but here is where sourced:
+              plugins = [
+                {
+                  name = "powerlevel10k";
+                  file = "powerlevel10k.zsh-theme";
+                  src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k";
+                }
+                {
+                  name = "zsh-vi-mode";
+                  src = "${pkgs.zsh-vi-mode}/share/zsh-vi-mode";
+                }
+                {
+                  name = "you-should-use";
+                  src = "${pkgs.zsh-you-should-use}/share/zsh/plugins/you-should-use";
+                }
+                {
+                  name = "fast-syntax-highlighting";
+                  src = "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions";
+                }
+              ];
+
+              history = {
+                size = cfg.historySize;
+                save = cfg.historySize;
+                path = "${config.xdg.stateHome}/zsh/history";
+
+                ignoreDups = true;
+                ignoreAllDups = true;
+                ignoreSpace = true;
+                share = true;
+              };
+              autocd = true;
+
+              initExtra = lib.concatStringsSep "\n\n" [
+                (common "init-extra.zsh")
+                "source ${zdotdir}/.p10k.zsh"
+              ];
+
+              syntaxHighlighting.enable = false; # Use zsh-fast-syntax-highlighting.
+              historySubstringSearch.enable = true;
+            };
+          };
+        };
     };
   };
 }
