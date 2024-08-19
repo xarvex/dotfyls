6cd801657f5cd83bcb057bcef415263ce1d8b828
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-19 04:03:16 -0500

    fix(mobilized): explicitly set monitor

--- hosts/botworks-mobilized/home.nix
+++ hosts/botworks-mobilized/home.nix
@@ -1,5 +1,15 @@
 { ... }:
 
 {
-  dotfyls.terminals.default = "kitty";
+  dotfyls = {
+    terminals.default = "kitty";
+
+    desktops.displays = [{
+      name = "eDP-1";
+      width = 1920;
+      height = 1200;
+      refresh = 60;
+      scale = 1.0;
+    }];
+  };
 }
