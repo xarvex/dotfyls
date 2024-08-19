0df7e9f0df96c97cf079d22dc5dee187b6189f2e
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-19 01:25:42 -0500

    feat(users)!: no default password

--- nixos/users.nix
+++ nixos/users.nix
@@ -1,26 +1,16 @@
-{ lib, user, ... }:
+{ user, ... }:
 
 {
-  options.warnings = lib.mkOption {
-    apply = lib.filter (
-      w: !(lib.strings.hasInfix "The options silently discard others by the order of precedence" w)
-    );
-  };
-
   config = {
     security.shadow.persistwd.enable = true;
 
     users = {
       mutableUsers = false;
       users = {
-        root = {
-          initialPassword = "password";
-          hashedPasswordFile = "/persist/etc/shadow/root";
-        };
+        root.hashedPasswordFile = "/persist/etc/shadow/root";
 
         ${user} = {
           isNormalUser = true;
-          initialPassword = "password";
           hashedPasswordFile = "/persist/etc/shadow/${user}";
           extraGroups = [ "networkmanager" "wheel" ];
         };
