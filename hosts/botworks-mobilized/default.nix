9e4c010b0884baf2b990c749b4560b6f349240d7
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-25 16:19:18 -0500

    feat(mobilized): disable wakeup from Logitech Bolt

--- hosts/botworks-mobilized/default.nix
+++ hosts/botworks-mobilized/default.nix
@@ -12,5 +12,7 @@
 
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="module", KERNEL=="i915", RUN+="${lib.getExe' pkgs.libdrm "proptest"} -M i915 -D /dev/dri/card1 322 connector 317 1"
+
+    ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
   '';
 }
