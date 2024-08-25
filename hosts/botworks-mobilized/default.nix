c69fe271708d49a7236251a1fe9a18897aaf8ad5
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-25 11:52:47 -0500

    feat(mobilized): configure external monitor

--- hosts/botworks-mobilized/default.nix
+++ hosts/botworks-mobilized/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, pkgs, ... }:
 
 {
   dotfyls = {
@@ -9,4 +9,8 @@
     security.harden.kernel.packages = false;
     filesystems.filesystems.zfs.unstable = true;
   };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="module", KERNEL=="i915", RUN+="${lib.getExe' pkgs.libdrm "proptest"} -M i915 -D /dev/dri/card1 322 connector 317 1"
+  '';
 }
--- hosts/botworks-mobilized/home.nix
+++ hosts/botworks-mobilized/home.nix
@@ -4,12 +4,21 @@
   dotfyls = {
     terminals.default = "kitty";
 
-    desktops.displays = [{
-      name = "eDP-1";
-      width = 1920;
-      height = 1200;
-      refresh = 60;
-      scale = 1.0;
-    }];
+    desktops.displays = [
+      {
+        name = "eDP-1";
+        width = 1920;
+        height = 1200;
+        refresh = 60;
+        scale = 1.0;
+      }
+      {
+        name = "HDMI-A-1";
+        width = 1920;
+        height = 1080;
+        refresh = 75;
+        position = "-1920x0";
+      }
+    ];
   };
 }
