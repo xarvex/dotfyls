810bfd7dfa70113d121f250475ded35caa07ae69
xarvex <gitlab-github.8qs1z@slmail.me>
2024-08-27 00:52:22 -0500

    feat(networking): configure Bluetooth

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -1,18 +1,32 @@
 { config, lib, ... }:
 
 # TODO: firewalls
+let
+  cfg = config.dotfyls.networking;
+in
 {
   options.dotfyls.networking = {
     wireless = lib.mkEnableOption "wireless networking" // { default = true; };
+    bluetooth = lib.mkEnableOption "Bluetooth networking" // { default = true; };
   };
 
-  config = lib.mkIf config.dotfyls.networking.wireless {
-    networking = {
-      # Pick only one of the below networking options.
-      # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-      networkmanager.enable = true; # Easiest to use and most distros use this by default.
-    };
+  config = lib.mkMerge [
+    (lib.mkIf cfg.wireless {
+      networking = {
+        # Pick only one of the below networking options.
+        # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+        networkmanager.enable = true; # Easiest to use and most distros use this by default.
+      };
 
-    dotfyls.persist.directories = [ "/etc/NetworkManager" ];
-  };
+      dotfyls.persist.directories = [ "/etc/NetworkManager" ];
+    })
+
+    (lib.mkIf cfg.bluetooth {
+      dotfyls.persist.directories = [ "/var/lib/bluetooth" ];
+
+      services.blueman.enable = true;
+
+      hardware.bluetooth.enable = true;
+    })
+  ];
 }
