c3c66b97314539a5eb72a8975a6a6d7c893b38f8
xarvex <gitlab-github.8qs1z@slmail.me>
2024-09-18 09:57:49 -0500

    feat(localization): better set language

--- nixos/localization.nix
+++ nixos/localization.nix
@@ -7,15 +7,18 @@ _:
     # Denmark (English) is closest to Sweden (English) which does not exist.
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_DK.UTF-8";
-      LC_IDENTIFICATION = "en_DK.UTF-8";
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "sv_SE.UTF-8";
       LC_MEASUREMENT = "en_DK.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
       LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_DK.UTF-8";
+      LC_NAME = "en_US.UTF-8";
       LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_DK.UTF-8";
-      LC_TELEPHONE = "en_DK.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
       LC_TIME = "en_DK.UTF-8";
+
+      LANGUAGE = "en_US:en:C:sv_SE:sv";
     };
   };
 }
