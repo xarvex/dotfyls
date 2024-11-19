5e65d00f8159528021bde6883521605844e77aa6
xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-11-18 20:06:10 -0600

    feat(nix): nh

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,39 +1,68 @@
-{ pkgs, ... }:
-
 {
-  nix = rec {
-    nixPath = [ "nixpkgs=flake:nixpkgs" ];
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}:
 
-    channel.enable = false;
+let
+  cfg = config.dotfyls.nix;
+in
+{
+  imports = [
+    (self.lib.mkAliasPackageModule
+      [
+        "dotfyls"
+        "nix"
+        "helper"
+      ]
+      [
+        "programs"
+        "nh"
+      ]
+    )
+  ];
 
-    package = pkgs.nixVersions.latest;
-
-    gc = {
-      automatic = true;
-      dates = "daily";
-      options = "--delete-older-than 14d";
-    };
-
-    settings = {
-      nix-path = nixPath;
-
-      auto-optimise-store = true;
-      use-xdg-base-directories = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-
-      substituters = [
-        "https://devenv.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
+  options.dotfyls.nix.helper.enable = lib.mkEnableOption "nh" // {
+    default = true;
   };
 
-  systemd.tmpfiles.rules = [ "R /nix/var/nix/profiles/per-user - - - 0" ];
+  config = {
+    nix = rec {
+      channel.enable = false;
+      package = pkgs.nixVersions.latest;
+      nixPath = [ "nixpkgs=flake:nixpkgs" ];
+
+      gc = {
+        automatic = true;
+
+        dates = "daily";
+        options = "--delete-older-than 14d";
+      };
+
+      settings = {
+        auto-optimise-store = true;
+        use-xdg-base-directories = true;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        nix-path = nixPath;
+
+        substituters = [
+          "https://devenv.cachix.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
+    };
+
+    programs.nh.enable = lib.mkIf cfg.helper.enable true;
+
+    systemd.tmpfiles.rules = [ "R /nix/var/nix/profiles/per-user - - - 0" ];
+  };
 }
