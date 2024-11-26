64d9c7409017e2542ccbb8f48f57a9176c85bd15
xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-11-25 23:00:03 -0600

    feat(desktops): complete GTK and Qt theming

--- home-manager/desktops/gtk.nix
+++ home-manager/desktops/gtk.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg' = config.dotfyls.desktops;
@@ -12,6 +17,21 @@ in
   config = lib.mkIf (cfg'.enable && cfg.enable) {
     gtk = {
       enable = true;
+
+      theme = {
+        name = "adw-gtk3-dark";
+        package = pkgs.adw-gtk3;
+      };
+      iconTheme = {
+        name = "Adwaita";
+        package = pkgs.adwaita-icon-theme;
+      };
+      font = {
+        inherit (config.dotfyls.fonts.sansSerif) name package;
+
+        # TODO: Set via shared option.
+        size = 11;
+      };
 
       gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
       gtk3.extraConfig = {
--- home-manager/desktops/qt.nix
+++ home-manager/desktops/qt.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   cfg' = config.dotfyls.desktops;
@@ -10,11 +15,64 @@ in
   };
 
   config = lib.mkIf (cfg'.enable && cfg.enable) {
+    home.packages = with pkgs; [
+      libsForQt5.qt5ct
+      libsForQt5.qtstyleplugin-kvantum
+      libsForQt5.qtwayland
+      qt6Packages.qt6ct
+      qt6Packages.qtstyleplugin-kvantum
+      qt6Packages.qtwayland
+    ];
+
     qt = {
       enable = true;
 
-      platformTheme.name = "adwaita";
-      style.name = "adwaita-dark";
+      platformTheme.name = "qtct";
+      style.name = "kvantum";
     };
+
+    xdg.configFile =
+      let
+        qtctConf = {
+          Appearance = {
+            custom_palette = false;
+            icon_theme = config.gtk.iconTheme.name;
+            standard_dialogs = "xdgdesktopportal";
+            style = "kvantum";
+          };
+        };
+
+        # TODO: Set via shared option.
+        defaultFont = "${config.dotfyls.fonts.sansSerif.name},11";
+      in
+      {
+        "Kvantum" = {
+          source = "${pkgs.catppuccin-kvantum.src}/themes";
+          recursive = true;
+        };
+        "Kvantum/kvantum.kvconfig".text = lib.generators.toINI { } {
+          # TODO: Change dynamically with heuniform.
+          General.theme = "catppuccin-mocha-maroon";
+        };
+
+        "qt5ct/qt5ct.conf".text = lib.generators.toINI { } (
+          qtctConf
+          // {
+            Fonts = rec {
+              fixed = ''"${defaultFont},-1,5,50,0,0,0,0,0"'';
+              general = fixed;
+            };
+          }
+        );
+        "qt6ct/qt6ct.conf".text = lib.generators.toINI { } (
+          qtctConf
+          // {
+            Fonts = rec {
+              fixed = ''"${defaultFont},-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
+              general = fixed;
+            };
+          }
+        );
+      };
   };
 }
