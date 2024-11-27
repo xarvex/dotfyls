7cd00fa6c2bd0fd10cc62ec8e9b29167cb46b97f
xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-11-27 05:03:20 -0600

    feat(kitty): configure

--- home-manager/terminals/kitty/default.nix
+++ home-manager/terminals/kitty/default.nix
@@ -36,9 +36,24 @@ in
         size = cfg'.fontSize;
       };
       settings = {
-        update_check_interval = 0;
+        # Scrollback
         scrollback_lines = 10000;
+
+        # Mouse
+        strip_trailing_spaces = "never";
+
+        # Terminal bell
+        enable_audio_bell = false;
+        window_alert_on_bell = false;
+
+        # Window layout
+        remember_window_size = false;
+
+        # Color scheme
         background_opacity = toString 0.85;
+
+        # Advanced
+        update_check_interval = 0;
       };
       extraConfig = ''
         include themes.conf
