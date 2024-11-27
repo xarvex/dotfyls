51845cdc623bb26e4b0beb9a33331c1415333d77
xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-11-27 17:29:08 -0600

    feat(media): Foliate

---
+++ home-manager/media/foliate/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}:
+
+let
+  cfg' = config.dotfyls.media;
+  cfg = cfg'.foliate;
+in
+{
+  options.dotfyls.media.foliate = {
+    enable = lib.mkEnableOption "Foliate" // {
+      default = config.dotfyls.desktops.enable;
+    };
+    package = lib.mkPackageOption pkgs "Foliate" { default = "foliate"; };
+  };
+
+  config = lib.mkIf (cfg'.enable && cfg.enable) {
+    dotfyls.persist.cacheDirectories = [
+      ".local/share/com.github.johnfactotum.Foliate"
+      ".cache/com.github.johnfactotum.Foliate"
+    ];
+
+    home.packages = [ (self.lib.getCfgPkg cfg) ];
+  };
+}
