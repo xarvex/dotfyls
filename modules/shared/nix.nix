4e1d72de56fb65670d8b8caf7ad9a61088f40b5f
xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-02 04:56:02 -0600

    fix(nix): settings permission warnings

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -1,41 +1,51 @@
 system:
 {
+  config,
   lib,
   pkgs,
   self,
   ...
 }:
 
+let
+  cfg = config.dotfyls.nix;
+in
 {
-  nix = rec {
-    package = if system then pkgs.nixVersions.latest else lib.mkDefault pkgs.nixVersions.latest;
-    nixPath = [ "nixpkgs=flake:nixpkgs" ];
-
-    gc = {
-      automatic = true;
-
-      options = "--delete-older-than 14d";
-    } // (lib.setAttrByPath [ (if system then "dates" else "frequency") ] "daily");
-
-    settings = {
-      auto-optimise-store = true;
-      use-xdg-base-directories = true;
-      experimental-features = [
-        "nix-command"
-        "flakes"
-      ];
-      nix-path = nixPath;
-
-      substituters = [
-        "https://devenv.cachix.org"
-        "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = [
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
+  options.dotfyls.nix = lib.optionalAttrs (!system) {
+    enableSettings = lib.mkEnableOption "configuring Nix settings";
   };
 
-  nixpkgs.overlays = [ self.overlays.default ];
+  config = {
+    nix = rec {
+      package = if system then pkgs.nixVersions.latest else lib.mkDefault pkgs.nixVersions.latest;
+      nixPath = [ "nixpkgs=flake:nixpkgs" ];
+
+      gc = {
+        automatic = true;
+
+        options = "--delete-older-than 14d";
+      } // (lib.setAttrByPath [ (if system then "dates" else "frequency") ] "daily");
+
+      settings = lib.mkIf (system || cfg.enableSettings) {
+        auto-optimise-store = true;
+        use-xdg-base-directories = true;
+        experimental-features = [
+          "nix-command"
+          "flakes"
+        ];
+        nix-path = nixPath;
+
+        substituters = [
+          "https://devenv.cachix.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
+    };
+
+    nixpkgs.overlays = [ self.overlays.default ];
+  };
 }
