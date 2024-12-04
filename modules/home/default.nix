fb67c8ad19730208c7677e49e473ba8d46c4b11e
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-03 22:37:28 -0600

    feat(files): move to module, expand on XDG dirs

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -17,25 +17,5 @@ _:
     ./state.nix
   ];
 
-  dotfyls.file = {
-    "Documents".persist = true;
-    "Pictures".persist = true;
-    "Videos".persist = true;
-
-    ".config".mode = "0700";
-
-    ".local".mode = "0700";
-
-    ".local/share".mode = "0700";
-    ".local/state".mode = "0700";
-
-    ".cache".mode = "0700";
-  };
-
   programs.home-manager.enable = true;
-
-  xdg = {
-    enable = true;
-    userDirs.enable = true;
-  };
 }
--- modules/home/files/default.nix
+++ modules/home/files/default.nix
@@ -8,4 +8,41 @@
 
     ./gvfs.nix
   ];
+
+  dotfyls.file = {
+    "Documents" = {
+      mode = "0700";
+      persist = true;
+    };
+    "Downloads" = {
+      mode = "0700";
+      cache = true;
+    };
+    "Music" = {
+      mode = "0700";
+      persist = true;
+    };
+    "Pictures" = {
+      mode = "0700";
+      persist = true;
+    };
+    "Videos" = {
+      mode = "0700";
+      persist = true;
+    };
+
+    ".config".mode = "0700";
+    ".local".mode = "0700";
+    ".local/share".mode = "0700";
+    ".local/state".mode = "0700";
+    ".cache".mode = "0700";
+  };
+
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
 }
