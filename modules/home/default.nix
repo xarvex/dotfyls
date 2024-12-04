94ce753155cb1256582a08e37f7fc6ad390ea085
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-03 19:12:09 -0600

    nit(state): separate

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -13,6 +13,7 @@
     ./terminals
 
     ./graphics.nix
+    ./state.nix
 
     self.homeManagerModules.nix
   ];
@@ -33,15 +34,6 @@
 
     ".cache/nix".cache = true;
   };
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   programs.home-manager.enable = true;
 
