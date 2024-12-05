58c7cafcde2811c00104cbb81896e4235e54b3cc
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-05 04:23:50 -0600

    feat(vesktop): use nixpkgs Vencord

--- modules/home/programs/vesktop/default.nix
+++ modules/home/programs/vesktop/default.nix
@@ -14,7 +14,10 @@ in
     enable = lib.mkEnableOption "Vesktop" // {
       default = config.dotfyls.desktops.enable;
     };
-    package = lib.mkPackageOption pkgs "Vesktop" { default = "vesktop"; };
+    package = lib.mkPackageOption pkgs "Vesktop" { default = "vesktop"; } // {
+      default = pkgs.vesktop.override { withSystemVencord = true; };
+      defaultText = "pkgs.vesktop.override { withSystemVencord = true; }";
+    };
   };
 
   config = lib.mkIf cfg.enable {
