db804e791da3fc4bc9e94080c57af08e9d073a79
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-11 02:47:20 -0600

    feat(users): persistwd update

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,4 +1,5 @@
 {
+  config,
   inputs,
   lib,
   user,
@@ -15,25 +16,29 @@
   };
 
   config = {
-    dotfyls.file."/etc/persistwd/shadow" = {
+    dotfyls.file.${config.security.shadow.persistwd.directory} = {
       mode = "0000";
       persist = true;
     };
 
-    security.shadow.persistwd.enable = true;
+    security.shadow.persistwd = {
+      enable = true;
+
+      users = [
+        "root"
+        user
+      ];
+      volume = lib.mkIf config.dotfyls.files.systems.impermanence.enable "/persist";
+    };
 
     users = {
       mutableUsers = false;
       users = {
-        root = {
-          initialPassword = "password";
-          hashedPasswordFile = "/etc/persistwd/shadow/root";
-        };
+        root.initialPassword = "password";
 
         ${user} = {
           isNormalUser = true;
           initialPassword = "password";
-          hashedPasswordFile = "/etc/persistwd/shadow/${user}";
           extraGroups = [
             "networkmanager"
             "wheel"
