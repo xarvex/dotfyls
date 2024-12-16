fdca67e4ec2b5fe739c2f054b064ef90ce564ca5
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-15 19:29:18 -0600

    feat(firefox): remove tmp containers

--- overlays/firefox/container-sort.sh
+++ overlays/firefox/container-sort.sh
@@ -7,7 +7,9 @@ if [ -f "${HOME}/.mozilla/firefox/profiles.ini" ]; then
     cd "${HOME}/.mozilla/firefox"
 
     while read -r path; do
-        jq --compact-output '.identities|=sort_by(.name)' "${path}/containers.json" |
+        jq --compact-output \
+            '.identities |= (map(select(.name // "tmp" | startswith("tmp") | not)) | sort_by(.name))' \
+            "${path}/containers.json" |
             sponge "${path}/containers.json"
     done <<EOF
 $(sed -n 's/^Path=\(.*\)$/\1/p' profiles.ini)
