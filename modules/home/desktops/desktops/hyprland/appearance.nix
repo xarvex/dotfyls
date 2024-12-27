0c4dc2ac69cb0080d17c5db151d0e4dc1be43229
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2024-12-26 23:45:30 -0600

    chore(flake): update

--- modules/home/desktops/desktops/hyprland/appearance.nix
+++ modules/home/desktops/desktops/hyprland/appearance.nix
@@ -56,6 +56,7 @@ lib.mkIf (cfg'.enable && cfg.enable) {
     misc = {
       disable_hyprland_logo = true;
       disable_splash_rendering = true;
+      disable_hyprland_qtutils_check = true;
     };
   };
 }
