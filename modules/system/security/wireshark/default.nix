dbfbad2488a0c6a7351d253bc24536b40b493873
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-01-06 23:38:13 -0600

    feat(wireshark): separate option for desktop application

--- modules/system/security/wireshark/default.nix
+++ modules/system/security/wireshark/default.nix
@@ -15,12 +15,17 @@ in
     (self.lib.mkAliasPackageModule [ "dotfyls" "security" "wireshark" ] [ "programs" "wireshark" ])
   ];
 
-  options.dotfyls.security.wireshark.enable = lib.mkEnableOption "Wireshark" // {
-    default = config.dotfyls.desktops.enable;
+  options.dotfyls.security.wireshark = {
+    enable = lib.mkEnableOption "Wireshark" // {
+      default = true;
+    };
+    application = lib.mkEnableOption "Wireshark application" // {
+      default = config.dotfyls.desktops.enable;
+    };
   };
 
   config = lib.mkIf cfg.enable {
-    dotfyls.security.wireshark.package = lib.mkDefault pkgs.wireshark;
+    dotfyls.security.wireshark.package = lib.mkIf cfg.application (lib.mkDefault pkgs.wireshark);
 
     programs.wireshark.enable = true;
 
