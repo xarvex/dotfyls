0831c87736365bff41ae858eb74d0c5c57a3fc10
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-01-18 03:15:25 -0600

    fix(hyprland): update rules

--- modules/home/desktops/desktops/hyprland/rules.nix
+++ modules/home/desktops/desktops/hyprland/rules.nix
@@ -6,19 +6,19 @@ let
 in
 lib.mkIf (cfg'.enable && cfg.enable) {
   wayland.windowManager.hyprland.settings.windowrulev2 = [
-    "float, class:^(firefox)$, title:^(Picture-in-Picture)$"
-    "keepaspectratio, class:^(firefox)$, title:^(Picture-in-Picture)$"
-    "pin, class:^(firefox)$, title:^(Picture-in-Picture)$"
+    "float, class:firefox, title:Picture-in-Picture"
+    "keepaspectratio, class:firefox, title:Picture-in-Picture"
+    "pin, class:firefox, title:Picture-in-Picture"
 
-    "dimaround, class:^(org.gnupg.pinentry-), floating: 1"
-    "stayfocused, class:^(org.gnupg.pinentry-)"
+    "dimaround, class:org\.gnupg\.pinentry-.+, floating: 1"
+    "stayfocused, class:org\.gnupg\.pinentry-.+"
 
-    "maxsize 1 1, class:^(xwaylandvideobridge)$"
-    "noanim, class:^(xwaylandvideobridge)$"
-    "noblur, class:^(xwaylandvideobridge)$"
-    "nofocus, class:^(xwaylandvideobridge)$"
-    "noinitialfocus, class:^(xwaylandvideobridge)$"
-    "opacity 0.0 override, class:^(xwaylandvideobridge)$"
+    "maxsize 1 1, class:xwaylandvideobridge"
+    "noanim, class:xwaylandvideobridge"
+    "noblur, class:xwaylandvideobridge"
+    "nofocus, class:xwaylandvideobridge"
+    "noinitialfocus, class:xwaylandvideobridge"
+    "opacity 0.0 override, class:xwaylandvideobridge"
 
     "suppressevent maximize, class:.*"
   ];
