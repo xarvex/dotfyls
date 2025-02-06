a9ac72f8a0baefd8302269ca05746e77251b7d38
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-02-06 15:57:10 -0600

    fix(nix): remove unmaintained cache

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -36,12 +36,10 @@ in
         nix-path = nixPath;
 
         substituters = [
-          "https://cuda-maintainers.cachix.org"
           "https://devenv.cachix.org"
           "https://nix-community.cachix.org"
         ];
         trusted-public-keys = [
-          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         ];
