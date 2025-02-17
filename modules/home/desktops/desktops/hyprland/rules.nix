2eef5e0f50c70372b18f758eb7c5afe489759d03
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-02-17 00:50:14 -0600

    feat(desktops/hyprland): center and size LibreOffice dialogs, spacing tweaks

--- modules/home/desktops/desktops/hyprland/rules.nix
+++ modules/home/desktops/desktops/hyprland/rules.nix
@@ -13,18 +13,21 @@ lib.mkIf (cfg'.enable && cfg.enable) {
       "pin, class:discord|vesktop, title:Discord Popout"
 
       "float, class:electron, title:Open Files"
-      "size <50% <50%, class:electron, title:Open Files"
+      "size 50% 50%, class:electron, title:Open Files"
 
       "float, class:firefox, title:Library|Picture-in-Picture"
       "keepaspectratio, class:firefox, title:Picture-in-Picture"
       "pin, class:firefox, title:Picture-in-Picture"
-      "size <50% <50%, class:firefox, title:Library"
+      "size 50% 50%, class:firefox, title:Library"
 
-      "dimaround, class:org\.gnupg\.pinentry-.+, floating: 1"
+      "center, class:soffice, floating:1"
+      "size 50% 50%, class:soffice, floating:1"
+
+      "dimaround, class:org\.gnupg\.pinentry-.+, floating:1"
       "stayfocused, class:org\.gnupg\.pinentry-.+"
 
       "float, class:xdg-desktop-portal-.+"
-      "size <50% <50%, class:xdg-desktop-portal-.+"
+      "size 50% 50%, class:xdg-desktop-portal-.+"
 
       "maxsize 1 1, class:xwaylandvideobridge"
       "noanim, class:xwaylandvideobridge"
