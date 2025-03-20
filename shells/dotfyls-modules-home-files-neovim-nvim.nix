d80b6ac6e8b3095cda070802f86efc1b1dfd9b68
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-03-20 02:43:11 -0500

    fix(shells): use correct hook

--- shells/dotfyls-modules-home-files-neovim-nvim.nix
+++ shells/dotfyls-modules-home-files-neovim-nvim.nix
@@ -2,25 +2,28 @@
 
 dotfyls.overrideAttrs (o: {
   nativeBuildInputs = (o.nativeBuildInputs or [ ]) ++ (with pkgs; [ luajit ]);
-  buildInputs = with pkgs; [
-    chafa
-    delta
-    fd
-    fzf
-    gcc
-    git
-    gnumake
-    neovim
-    nodejs_22
-    ripgrep
-  ];
+  buildInputs =
+    (o.buildInputs or [ ])
+    ++ (with pkgs; [
+      chafa
+      delta
+      fd
+      fzf
+      gcc
+      git
+      gnumake
+      neovim
+      nodejs_22
+      ripgrep
+    ]);
 
-  enterShell =
-    (o.enterShell or "")
+  shellHook =
+    (dotfyls.shellHook or "")
+    # bash
     + ''
-      NVIM_APPNAME="dotfyls/devshell/nvim/$(git log -1 --format=%h @{push})"
+      NVIM_APPNAME=dotfyls/devshell/nvim/"$(git log -1 --format=%h @{push})"
       export NVIM_APPNAME
-      mkdir -p "''${XDG_CONFIG_HOME:-''${HOME}/.config}/dotfyls/devshell/nvim"
-      ln -fsT "''${PWD}" "''${XDG_CONFIG_HOME:-''${HOME}/.config}/''${NVIM_APPNAME}"
+      mkdir -p "''${XDG_CONFIG_HOME:-''${HOME}/.config}"/dotfyls/devshell/nvim
+      ln -fsT "''${PWD}" "''${XDG_CONFIG_HOME:-''${HOME}/.config}"/"''${NVIM_APPNAME}"
     '';
 })
