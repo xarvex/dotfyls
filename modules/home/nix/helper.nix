041934a066de49f01163be6643011d66a21171f4
Xarvex <dev.ellz6@xarvex.simplelogin.com>
2025-04-27 03:47:16 -0500

    chore(flake): update

--- modules/home/nix/helper.nix
+++ modules/home/nix/helper.nix
@@ -9,8 +9,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.sessionVariables = lib.optionalAttrs config.dotfyls.files.sync.enable {
-      FLAKE = "${config.xdg.userDirs.documents}/Projects/dotfyls";
+    home.sessionVariables = lib.mkIf config.dotfyls.files.sync.enable {
+      NH_FLAKE = "${config.xdg.userDirs.documents}/Projects/dotfyls";
     };
 
     programs.nh.enable = true;
